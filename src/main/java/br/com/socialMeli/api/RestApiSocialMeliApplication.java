47906c32113f377ec83968946cf8c1591abc5c2b
Amedeo Elmo <amedeo.elmo@mercadolivre.com>
2021-06-01 14:26:04 -0300

    Starts the project w Spring Initializr

---
+++ src/main/java/br/com/socialMeli/api/RestApiSocialMeliApplication.java
@@ -0,0 +1,13 @@
+package br.com.socialMeli.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSocialMeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSocialMeliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/socialMeli/api/RestApiSocialMeliApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.socialMeli.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSocialMeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
